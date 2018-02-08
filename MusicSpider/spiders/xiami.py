f33ba361e0ab6a4baa78395593c27ce2ff6fe9f7
胡茜茜 <huxixi@limefamily.com>
2018-02-08 15:01:09 +0800

    最近收听的歌曲和歌曲对应的歌手

--- MusicSpider/spiders/xiami.py
+++ MusicSpider/spiders/xiami.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
+# strip replace("1", "2")
+#//span[contains(@class, "vote-post")]
 import scrapy
 
 
 class XiamiSpider(scrapy.Spider):
     name = 'xiami'
     allowed_domains = ['xiami.com']
-    start_urls = ['http://www.xiami.com/']
+    start_urls = ['http://www.xiami.com/u/53530979?spm=0.0.0.0.NOaCka']
 
     def parse(self, response):
-        pass
+        recent_songs = response.xpath("//td[@class='song_name']")
+        songs = recent_songs.xpath("a[1]/text()").extract()
+        singers = recent_songs.xpath("a[2]/text()").extract()
+        for i in songs:
+            print(i)
+        for j in singers:
+            print(j)
+
